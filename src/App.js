3bc0cb397e0454d7c0a64f1494e7727591503d75
maninder-kaur-21 <sainimaninder733@gmail.com>
2023-10-19 17:23:03 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="cover">
+      <div className="cover-text">
+        <p>
+          Hi, Maninder Kaur here
+        </p>
+        <p>
+          I'm Software Developer.
+        </p>        
+      </div>
+    </div>
+  );
+}
+
+export default App;
